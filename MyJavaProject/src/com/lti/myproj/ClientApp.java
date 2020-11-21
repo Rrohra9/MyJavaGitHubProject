9193b6041053f0a8491aca11437f68c49fcbff08
Rrohra9 <44185277+Rrohra9@users.noreply.github.com>
2020-11-21 11:16:08 +0530

    Update ClientApp.java

--- MyJavaProject/src/com/lti/myproj/ClientApp.java
+++ MyJavaProject/src/com/lti/myproj/ClientApp.java
@@ -1,11 +1,10 @@
 package com.lti.myproj;
-import com.lti.myproj.Person;
 public class ClientApp {
 
 	
 	public static void main(String[] args) {
-		Person p = new Person ("rohit", 111, 2222);
-		System.out.println(p);
+		//Person p = new Person ("rohit", 111, 2222);
+		//System.out.println(p);
 		System.out.println("rohit");
 	}
 }
